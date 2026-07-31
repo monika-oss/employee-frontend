11c8f8f4bba475414120b6cca2e3bc58e9f3780a
monika <monikababu180505@gmail.com>
2026-07-31 21:39:20 +0530

    correct backend url

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
     ? 'http://localhost:5000/api' 
-    : 'https://employee-backend-1zst.onrender.com/api';
+    : 'https://employee-backend-fgok.onrender.com/api';
 
 const apiFetch = async (endpoint, options = {}) => {
     const token = localStorage.getItem('token');
